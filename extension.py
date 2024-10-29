6bf88d5d36fcfcf8c81de63a6c21425883ade148
Sebastian Cisneros <sebastian.cisneros384@gmail.com>
2024-10-29 12:54:47 -0300

    fix: parse json

--- extension.py
+++ extension.py
@@ -1,12 +1,13 @@
+import json
 from copier_templates_extensions import ContextHook
 
 class ExtraEnvExtension(ContextHook):
   def hook(self, context):
-    print("ExtraEnvExtension hook", context)
     extra_env = context.get('_extra_env', {})
 
     # Add the extra environment variables to self.environment
     if extra_env:
+      extra_env = json.loads(extra_env) if isinstance(extra_env, str) else extra_env
       for key, value in extra_env.items():
         self.environment[key] = value
     

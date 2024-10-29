338761c902fa56f16a85cb40237ccc8eb0a4959a
Sebastian Cisneros <sebastian.cisneros384@gmail.com>
2024-10-29 13:00:30 -0300

    add: print debug

--- extension.py
+++ extension.py
@@ -7,7 +7,9 @@ class ExtraEnvExtension(ContextHook):
 
     # Add the extra environment variables to self.environment
     if extra_env:
+      print(extra_env)
       extra_env = json.loads(extra_env) if isinstance(extra_env, str) else extra_env
+      print(extra_env)
       for key, value in extra_env.items():
         self.environment[key] = value
     
